2d3e13fcc0acf5c5aacb0b5c064fb64088dd90a2
Gustavo de Souza Silva <125916967+Gustavo127400@users.noreply.github.com>
2023-10-20 12:31:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const scoreDolphins = 96 + 108 + 89 / 3;
+const scoreKoalas = 88 + 91 + 110 / 3;
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else {
+  console.log("Koalas win the trophy");
+}
+if (scoreDolphins == scoreKoalas) {
+  console.log("Both win the trophy");
+}
